660c820e0618ade97a1b1e33bb3f3848a0a56ce4
bsjojo <beabusamuel@gmail.com>
2023-09-08 03:44:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - this function is the entry point for the program
+ *
+ *Return: used to exit a function, always 0
+*/
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
