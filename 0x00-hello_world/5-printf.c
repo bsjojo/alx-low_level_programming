32bac8bb4df2bdf348e2290488b45da558c8a9d9
bsjojo <beabusamuel@gmail.com>
2023-09-08 17:04:28 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this function is the entry point,the main value is void
+ *
+ * return: it is used to exit a function, it is always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
