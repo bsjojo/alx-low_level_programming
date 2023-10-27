17b6fb220b34924580bac65765a2406525d85e8e
bsjojo <beabusamuel@gmail.com>
2023-10-27 05:50:03 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow - func calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int s;
+
+	num = 1;
+	for (s = 1; s <= power; s++)
+		num *= base;
+	return (num);
+}
+
